b1a3a9c2c8131110c7c461cc7b720ba0481eae2c
joanbarres <150377040+joanbarres@users.noreply.github.com>
2023-11-23 18:04:52 +0100

    Update ej2.py

---
+++ ej2.py
@@ -0,0 +1,8 @@
+nom = input("Introdueix el teu nom: ")
+dni = input("Introdueix el teu DNI: ")
+edat = int(input("Introdueix la teva edat: "))  
+
+print("\nValors introduïts:")
+print("Nom:", nom)
+print("DNI:", dni)
+print("Edat:", edat)
